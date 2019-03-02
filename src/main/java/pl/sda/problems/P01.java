64544bf77a10b7b451b5448fde581fd798f0a761
Pandragona <asiuspospolitus@gmail.com>
2019-03-02 21:30:34 +0100

    dropEveryNth, split, slice, insertAt

--- src/main/java/pl/sda/problems/P01.java
+++ src/main/java/pl/sda/problems/P01.java
@@ -1,18 +1,129 @@
 package pl.sda.problems;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class P01 {
 
-    public static String last(List<String> list){
-        return list.get(list.size()-1);
+    public static String last(List<String> list) {
+        return list.get(list.size() - 1);
     }
 
-//    public static List<Integer> reverse(List<Integer> number){
+    public static List<Integer> reverse(List<Integer> number) {
+
+        List<Integer> reversed = new ArrayList<>(number);
+
+        Collections.reverse(reversed);
+
+        return reversed;
+    }
+
+    public static boolean isPalindromeWord(String originalWord) {
+
+        String reversed = new StringBuffer(originalWord).reverse().toString();
+
+        if (originalWord.equals(reversed)) {
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+
+//    public static boolean isPalindrome(List<String> original){
 //
-//        return number.reve
+//        List<String> reversed = new ArrayList<>(original);
+//
+//        Collections.reverse(reversed);
+//
+//        if (original.equals(reversed)){
+//            return true;
+//
+//        }else {
+//
+//            return false;
+//        }
 //    }
 
+    public static boolean isPalindrome(List<Integer> original) {
 
+        List<Integer> reversed = new ArrayList<>(original);
+
+        Collections.reverse(reversed);
+
+        if (original.equals(reversed)) {
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+
+
+    public static List<String> dropEveryNth(List<String> originalList, int j) {
+        List<String> newList = new ArrayList<>();
+
+        for (int i = 0; i < originalList.size(); i++) {
+            if ((i + 1) % j != 0) {
+                newList.add(originalList.get(i));
+            }
+        }
+
+        return newList;
+    }
+
+    public static Map<Boolean, List<String>> split(List<String> originalList, int j) {
+
+        Map<Boolean, List<String>> map = new HashMap<>();
+
+        List<String> firstHalf = new ArrayList<>();
+        List<String> secondHalf = new ArrayList<>();
+
+        for (int i = 0; i < originalList.size(); i++) {
+            if (i < j) {
+                firstHalf.add(originalList.get(i));
+            } else {
+                secondHalf.add(originalList.get(i));
+            }
+        }
+
+        map.put(true, firstHalf);
+        map.put(false, secondHalf);
+
+        return map;
+    }
+
+    public static List<String> slice(List<String> originalList, int j, int k) {
+        List<String> slicedList = new ArrayList<>();
+
+        for (int i = 0; i < originalList.size(); i++) {
+            if (i > (j-2) && i < (k)) {
+                slicedList.add(originalList.get(i));
+            }
+        }
+
+        return slicedList;
+    }
+
+    public static List<String> insertAt(List<String> input, int i, String alfa) {
+        List<String> output = new ArrayList<>(input);
+
+            output.add(i, alfa);
+
+        return output;
+    }
+
+    public static List<String> flatten(List<Object> asList, Class<String> stringClass) {
+        List<String> flatten = new ArrayList<>();
+
+        for (Object e: asList) {
+            if ()
+        }
+        //instanceOf
+
+        return flatten;
+    }
 }
--- src/test/java/pl/sda/problems/Problems.java
+++ src/test/java/pl/sda/problems/Problems.java
@@ -4,14 +4,19 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertFalse;
-import pl.sda.problems.P01;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 public class Problems {
 
@@ -30,9 +35,46 @@ public class Problems {
 //    public void shouldReturnTrueWhenListIsPalindrome() throws Exception {
 //        assertTrue(P01.isPalindrome(Arrays.asList("x", "a", "m", "a", "x")));
 //    }
-//
-//    @Test
-//    public void shouldReturnFalseWhenListIsNotPalindrome() throws Exception {
-//        assertFalse(P01.isPalindrome(Arrays.asList(1, 2, 3, 4, 5)));
-//    }
+
+    @Test
+    public void shouldReturnFalseWhenListIsNotPalindrome() throws Exception {
+        assertFalse(P01.isPalindrome(Arrays.asList(1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void shouldFlattenAListOfList() throws Exception {
+        List<String> flatten = P01.flatten(asList("a", asList("b", asList("c", "d")), "e"), String.class);
+        assertThat(flatten, hasSize(5));
+        assertThat(flatten, hasItems("a", "b", "c", "d", "e"));
+    }
+
+    @Test
+    public void shouldDropEveryNthElement() throws Exception {
+        List<String> result =
+                P01.dropEveryNth(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), 3);
+        assertThat(result, hasSize(8));
+        assertThat(result, contains("a", "b", "d", "e", "g", "h", "j", "k"));
+    }
+    @Test
+    public void shouldSplitInTwoHalves() throws Exception {
+        Map<Boolean, List<String>> result =
+                P01.split(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"), 3);
+        assertThat(result.get(true), contains("a", "b", "c"));
+        assertThat(result.get(false), contains("d", "e", "f", "g", "h", "i", "k"));
+    }
+
+    @Test
+    public void shouldGiveSliceOfAList() throws Exception {
+        List<String> slice = P01.slice(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"), 3, 7);
+        assertThat(slice, hasSize(5));
+        assertThat(slice, contains("c", "d", "e", "f", "g"));
+    }
+
+    @Test
+    public void shouldInsertElementAtSecondPosition() throws Exception {
+        List<String> input = Stream.of("a", "b", "c", "d").collect(toList());
+        List<String> result = P01.insertAt(input, 2, "alfa");
+        assertThat(result, hasSize(5));
+        assertThat(result, contains("a", "alfa", "b", "c", "d"));
+    }
 }
